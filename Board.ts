e69550263f2b12deb90ddb1f9a3d322af1632e90
jzingh@ucdavis.edu <jzingh@ucdavis.edu>
2020-08-26 06:23:43 -0700

    Initial commit

---
+++ Board.ts
@@ -0,0 +1,10 @@
+
+class Board {
+    private height: number;
+    private width: number;
+    private state: number[][];
+    constructor() {
+        this.height = 8;
+        this.width = 8;
+    }
+}
